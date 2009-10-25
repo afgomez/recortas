2363edfe6cab8753fbe563873441d9128e00eb2c
Alejandro Fernández <antarticonorte@gmail.com>
2009-10-25 19:26:23 +0100

    Added some tests to Recortas::decode
    * Changed 'url' for 'uri' as parameter
    * Added custom Exception class
    * Raises exception if uri is not from http://cort.as

--- lib/recortas.rb
+++ lib/recortas.rb
@@ -1,19 +1,20 @@
 require 'net/http'
 require 'rexml/document'
 
+class URIError < ArgumentError; end
+
 class Recortas
   
-  def self.decode(url)
+  def self.decode(uri)
+    raise URIError, "Not a valid cort.as URI" unless uri =~ /^http:\/\/cort\.as/
     
-    xml_data = Net::HTTP.get_response(URI.parse("#{url}.xml")).body
+    xml_data = Net::HTTP.get_response(URI.parse("#{uri}.xml")).body
     xml_doc = REXML::Document.new(xml_data)
-    
+
     if xml_doc.root.children[0].text == 'ok'
       xml_doc.root.children[1].text
     else
-      raise ArgumentError, REXML::XPath.first(xml_doc, '//errorLong').text
+      raise URIError, REXML::XPath.first(xml_doc, '//errorLong').text
     end
-    
-    
   end
 end
