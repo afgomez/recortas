673c6e4b411b1aeb9f1ab5e05e43efb8a303b1c9
Alejandro Fernández <antarticonorte@gmail.com>
2009-10-25 19:53:45 +0100

    Added method Recortas::encode(uri)
    Encodes the uri param

--- lib/recortas.rb
+++ lib/recortas.rb
@@ -6,13 +6,26 @@ class URIError < ArgumentError; end
 class Recortas
   
   def self.decode(uri)
-    raise URIError, "Not a valid cort.as URI" unless uri =~ /^http:\/\/cort\.as/
+    raise URIError, "Not a valid cort.as URI" unless uri =~ /^http:\/\/cort\.as/i
     
     xml_data = Net::HTTP.get_response(URI.parse("#{uri}.xml")).body
     xml_doc = REXML::Document.new(xml_data)
 
     if xml_doc.root.children[0].text == 'ok'
       xml_doc.root.children[1].text
+    else
+      raise URIError, REXML::XPath.first(xml_doc, '//errorLong').text
+    end
+  end
+  
+  def self.encode(uri)
+    uri = "http://" + url unless uri =~ /^http:\/\//i
+    
+    xml_data = Net::HTTP.get_response(URI.parse("http://www.soitu.es/cortas/encode.pl?r=xml&u=#{uri}")).body
+    xml_doc = REXML::Document.new(xml_data)
+    
+    if xml_doc.root.children[0].text == 'ok'
+      REXML::XPath.first(xml_doc, '//urlCortas').text
     else
       raise URIError, REXML::XPath.first(xml_doc, '//errorLong').text
     end
--- usage.rb
+++ usage.rb
@@ -3,16 +3,5 @@ require 'lib/recortas'
 # Decoding an URI
 puts Recortas::decode('http://cort.as/1')
 
-# Invalid short code
-begin
-  puts Recortas::decode('http://cort.as/111111111111111')
-rescue URIError => e
-  puts "Err: #{e}"
-end
-
-# Invalid cort.as URI
-begin
-  puts Recortas::decode('http://google.com')
-rescue URIError => e
-  puts "Err: #{e}"
-end
+# Encoding an URI
+puts Recortas::encode('http://www.yahoo.com/')
