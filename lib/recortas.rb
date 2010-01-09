faf336fb3b989cdf651e187d80b55cb8b5945311
Alejandro Fernández <antarticonorte@gmail.com>
2010-01-09 08:15:32 +0100

    Moved recortas.rb to the lib directory

--- lib/recortas.rb
+++ lib/recortas.rb
@@ -1,26 +1,26 @@
-# = re.cort.as A ruby wrapper for the http://cort.as service
+# = recortas A ruby wrapper for the http://cort.as service
 #
-# re.cort.as is a basic ruby wrapper for the http://soitu.es http://cort.as service.
+# recortas is a basic ruby wrapper for the http://soitu.es http://cort.as service.
 #
 # Currently only have two methods: one for decoding and one for encoding an URI.
 # Support for statistics is planned
 #
 # == Encoding an URI
 # 
-#     Recortas::encode(uri)
+#     Recortas.encode(uri)
 # 
 # Returns a string with the shortened URI
 # 
 # == Decoding an cort.as URI
 #     
-#     Recortas::decode(uri)
+#     Recortas.decode(uri)
 # 
 # Returns a string with the original URI. The uri parameter must be a valid cort.as uri (i.e. must start with 'http://cort.as')
 # 
 # If an error occurs, an exception of class CortasError is raised.
 # 
 #     begin
-#       Recortas::decode('http://www.google.com')
+#       Recortas.decode('http://www.google.com')
 #     rescue CortasError => e
 #       puts e # Not a valid cort.as URI
 #     end
@@ -66,7 +66,7 @@ class Recortas
   # Get the original URI shortened with cort.as as a string
   #
   # Usage example
-  #   original = Recortas::decode('http://cort.as/1')   # Returns 'http://www.yahoo.com/'
+  #   original = Recortas.decode('http://cort.as/1')   # Returns 'http://www.yahoo.com/'
   # 
   # Raises CortasError when +uri+ is not a cort.as URI
   
@@ -87,12 +87,12 @@ class Recortas
   # Sends an URI to http://cort.as to be shortened
   #
   # Usage example
-  #   short = Recortas::encode('http://www.yahoo.com')  # Returns 'http://cort.as/1'
+  #   short = Recortas.encode('http://www.yahoo.com')  # Returns 'http://cort.as/1'
   #
   # Raises CortasError if the +uri+ cannot be shortened
   #
   #   begin
-  #     short = Recortas::encode('blalal')
+  #     short = Recortas.encode('blalal')
   #   rescue CortasError => e
   #     puts e  # Error returned from cort.as service
   #   end
